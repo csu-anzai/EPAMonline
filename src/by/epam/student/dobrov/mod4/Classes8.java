0415f89b7c28a189e7fe50d4330fc52e00b25403
Victor <Artofpaganini@gmail.com>
2019-09-04 11:53:10 +0300

    init

--- src/by/epam/student/dobrov/mod4/Classes8.java
+++ src/by/epam/student/dobrov/mod4/Classes8.java
@@ -17,7 +17,7 @@ public class Classes8 {
 
         Customer[] customers = new Customer[3];
         customers[0] = new Customer(111, "Qwert7", "Qwert1", "Qwert1", "Qwe1", 1111, 1111);
-        customers[1] = new Customer(222, "Qwert2", "Qwert2", "Qwert2", "Qwe2", 2222, 2222);
+        customers[1] = new Customer(222, "Qwert1", "Qwert2", "Qwert2", "Qwe2", 2222, 2222);
         customers[2] = new Customer(333, "Qwert3", "Qwert3", "Qwert3", "Qwe3", 3333, 3333);
 
 
@@ -122,7 +122,7 @@ class Customer {
                 '}');
     }
 
-    public boolean checkSurname() {
+    public boolean checkSurname(String surname) {
         if (surname == this.surname) {
             return true;
         }
@@ -131,7 +131,8 @@ class Customer {
 }
 
 class DataCustomers {
-    private Customer[] customers;
+    private static Customer[] customers;
+//    private Customer[] customers;
 
 
     public DataCustomers(Customer... customers) {
@@ -151,11 +152,11 @@ class DataCustomers {
     }
 
     //список покупателей в алфавитном порядке;
-    public Customer[] sortBySurname() {
+    public  static Customer[] sortBySurname() {
 
         Customer temp;
 
-        for (int i = this.customers.length - 1; i > 0; i--) {
+        for (int i = customers.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (customers[j].getSurname().compareTo(customers[j + 1].getSurname()) > 0) {
                     temp = customers[j];
@@ -170,7 +171,7 @@ class DataCustomers {
     }
 
     //список покупателей, у которых номер кредитной карточки находится в заданном интервале
-    public void dataCustomersByCreditCard(int a, int b) {
+    public Customer[] dataCustomersByCreditCard(int a, int b) {
 
         for (int i = 0; i < this.customers.length; i++) {
             if (customers[i].getNumberOfCreditCard() >= a && customers[i].getNumberOfCreditCard() <= b) {
@@ -178,14 +179,15 @@ class DataCustomers {
             }
         }
 
+        return customers;
     }
 
 
     //вывод на консоль
-    public void showCustomerBySurname() {
+    public  static void showCustomerBySurname() {
 
-        for (int i = 0; i < this.customers.length; i++) {
-            System.out.println(this.customers[i]);
+        for (int i = 0; i < customers.length; i++) {
+            System.out.println(customers[i]);
         }
 
     }
